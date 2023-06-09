cad48046422aaefeab87ebdae687dd50679fc7ea
sabbir1257 <masabbir1257@gmail.com>
2023-06-09 22:55:26 +0600

    class get section push

--- index.js
+++ index.js
@@ -9,3 +9,61 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihnyz1z.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classCollection = client.db("yoga").collection("class");
+    const instructorsCollection = client.db("yoga").collection("instructors");
+
+
+    app.get('/classes', async (req, res) => {
+        const classes = await classCollection.find().sort({
+            student_enroll: -1
+        }).toArray();
+        res.send(classes);
+    })
+
+    app.get('/top-classes', async (req, res) => {
+        const classes = await classCollection.find().limit(6).sort({
+            student_enroll: -1
+        }).toArray();
+        res.send(classes);
+    })
+
+    
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("boss is running");
+  });
+  
+  app.listen(port, (req, res) => {
+    console.log(`Bistro Boss is running on port: ${port}`);
+  });
